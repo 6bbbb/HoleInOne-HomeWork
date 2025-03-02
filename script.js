e38c1693d93981aa0ea4697a28d206ce202f2ee0
6bbbb <ayham078522@gmail.com>
2025-03-02 11:44:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Typed
+var typed = new Typed(".typed",{
+    strings:[
+        "Welcome To Hole in One Website Enjoy!",
+       "مرحبا بك في موقع جولف هول ان ون حيث يجتمع المرح , استمتع!"
+    ],
+    typeSpeed :70,
+    backSpeed :40,
+    loop:true
+})
