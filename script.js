18100f486fe2f7f8f45f9fb52a6cea0ca8ad09ac
AYHAM <161080891+6bbbb@users.noreply.github.com>
2025-03-03 19:57:12 +0300

    Ready V1

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // Typed
-var typed = new Typed(".typed",{
-    strings:[
+var typed1 = new Typed(".typed", {
+    strings: [
         "Welcome To Hole in One Website Enjoy!",
-       "مرحبا بك في موقع جولف هول ان ون حيث يجتمع المرح , استمتع!"
+        "مرحبا بك في موقع جولف هول ان ون حيث يجتمع المرح , استمتع!"
     ],
-    typeSpeed :70,
-    backSpeed :40,
-    loop:true
-})
+    typeSpeed: 70,
+    backSpeed: 40,
+    loop: true
+});
+
+// Show Side bar
+
+function chowSidebar() {
+    const sidebarItems = document.querySelectorAll(".side-bar li");
+    sidebarItems.forEach(item => {
+        item.style.display = 'block';
+    });
+}
+
+function hideSidebar() {
+    const sidebarItems = document.querySelectorAll(".side-bar li");
+    sidebarItems.forEach(item => {
+        item.style.display = 'none';
+    });
+}
+
